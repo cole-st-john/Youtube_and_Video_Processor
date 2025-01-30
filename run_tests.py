5999db7419b7d9ccfe652f67398f31166662ba2a
cole-st-john <info@colestjohn.com>
2025-01-30 20:20:53 +0100

    draft testing improvements

--- run_tests.py
+++ run_tests.py
@@ -1,12 +1,30 @@
 import os
+import subprocess
 import sys
 
-SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(os.path.dirname(SCRIPT_DIR))
+SCRIPT_DIR = os.getcwd()
 
-os.chdir(SCRIPT_DIR)
-cmd_str = ". ./.venv/bin/activate && coverage run -m pytest ."
-os.system(cmd_str)
+# Define paths
+venv_activate = os.path.join(SCRIPT_DIR, ".venv", "Scripts", "activate.bat")
+pytest_cmd = f'"{venv_activate}" & coverage run -m pytest .'
 
-# Viewing Coverage
-os.system("coverage html")
+# Print and execute the command
+print(f"Running tests: {pytest_cmd}")
+
+try:
+    subprocess.run(pytest_cmd, shell=True, check=True)
+    print("Generating coverage report...")
+    subprocess.run("coverage html", shell=True, check=True)
+    print("Coverage report generated successfully.")
+except subprocess.CalledProcessError as e:
+    print(f"Error: {e}")
+    sys.exit(1)
+
+# Open coverage report
+open_report = os.path.join(SCRIPT_DIR, "htmlcov", "index.html")
+pytest_cmd = f'start "" "{open_report}"'
+try:
+    subprocess.run(pytest_cmd, shell=True, check=True)
+except subprocess.CalledProcessError as e:
+    print(f"Error: {e}")
+    sys.exit(1)
--- tests/test_yt_dler.py
+++ tests/test_yt_dler.py
@@ -1,31 +1,47 @@
 import os
 import sys
-
-from video_processor import app
-
-# import unittest
+import importlib.metadata
 import pytest
 
-# Work-around as non-installed package
-SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(os.path.dirname(SCRIPT_DIR))
+# import inspect
+# import video_processor
+from video_processor import app, media  # , ffmpeg_tools, video_job_gui
+
+# Checking test dependencies
+dependencies = ["pytest-env"]
+
+
+def check_dependencies(dependencies):
+    def pkg_installed(package_name):
+        try:
+            importlib.metadata.version(package_name)
+            return True
+        except importlib.metadata.PackageNotFoundError:
+            return False
+
+    missing_dependencies = [pkg for pkg in dependencies if not pkg_installed(pkg)]
+    if any(missing_dependencies):
+        raise ImportError(f"Missing some package dependencies: {'\n'.join(missing_dependencies)}")
+
+
+# using env variable in pyproject.toml and pytest-env
+check_dependencies(dependencies)
 
 
 # different tests resources
+TEST_DIR = os.path.dirname(os.path.abspath(__file__))
+
 EXAMPLE_VIDEO_1 = "https://youtu.be/BsGjkkPKbsk"
-SHORT_VIDEO = "https://www.youtube.com/shorts/zthRM9pI6TQ"
-LONG_VIDEO = "https://www.youtube.com/shorts/o5Q7GShNo3Q"
-VIDEO_IN_PLAYLIST = "https://www.youtube.com/shorts/o5Q7GShNo3Q"
+SHORT_VIDEO_1 = "https://www.youtube.com/shorts/zthRM9pI6TQ"
+SHORT_VIDEO_2 = "https://www.youtube.com/shorts/o5Q7GShNo3Q"
+VIDEO_IN_PLAYLIST_1 = "https://www.youtube.com/shorts/o5Q7GShNo3Q"
 PLAYLIST = "https://www.youtube.com/shorts/o5Q7GShNo3Q"
 INVALID_VIDEO = "https://www.youtube.com/shorts/o5Q7GShNo3Q"
 LOW_QUALITY_VIDEO = "https://www.youtube.com/shorts/o5Q7GShNo3Q"
 ULTRA_HD_VIDEO = "https://www.youtube.com/shorts/o5Q7GShNo3Q"
-LOCAL_VIDEO_VALID = os.path.join(SCRIPT_DIR, "test_resources", "video_1.mp4")
-LOCAL_VIDEO_INVALID = os.path.join(SCRIPT_DIR, "test_resources", "video_not_existing.mp4")
+LOCAL_VIDEO_VALID = os.path.join(TEST_DIR, "test_resources", "video_1.mp4")
+LOCAL_VIDEO_INVALID = os.path.join(TEST_DIR, "test_resources", "video_not_existing.mp4")
 
-
-# unittest.TestCase
-# def setUp(self):
 
 # testing gui
 # testing dl functionality
@@ -53,3 +69,34 @@ class Test_Integration:
 # configuration ====================
 class Test_Config_Gui:
     pass
+
+
+# Job processing
+class Test_Job_Processing:
+    def test_job_processing_1(self):
+        new_job = media.Job()
+        new_job.url = "https://www.youtube.com/shorts/zthRM9pI6TQ"
+        # new_job.name = "async1"
+        new_job.name = f"{sys._getframe().f_code.co_name}"
+        new_job.stop_event = False
+        media.process_job_async(new_job)
+        assert 1 == 1
+
+    def test_job_processing_2(self):
+        new_job = media.Job()
+        new_job.url = "https://www.youtube.com/shorts/zthRM9pI6TQ"
+        new_job.name = f"{sys._getframe().f_code.co_name}"
+        new_job.stop_event = False
+        success_flag = media.process_job_sync(new_job)
+        assert success_flag is True
+
+    def test_job_processing_3(self):
+        new_job = media.Job()
+        new_job.url = "https://www.youtube.com/shorts/zthRM9pI6TQ"
+        new_job.name = f"{sys._getframe().f_code.co_name}"
+        new_job.start_time = 1
+        new_job.end_time = 2
+        new_job.cover_time = 0
+        new_job.speed_mult = 0.5
+        success_flag = media.process_job_sync(new_job)
+        assert success_flag is True
